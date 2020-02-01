8aff32717f523e9291d219464b7f505cea1d850d
raviraj <raviraj437437@gmail.com>
2020-02-01 17:33:26 +0530

    add high and low temp to a forcast

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -8,7 +8,8 @@ const forcast=(latitude,longitude,callback)=>{
             callback('unable to find location.Try another search',undefined)
         }
         else{
-            callback(undefined,body.daily.data[0].summary+" It is currently "+body.currently.temperature+" degree out.There is a "+body.currently.precipIntensity+" % chance of rain")
+           // console.log(body.daily.data[0])
+            callback(undefined,body.daily.data[0].summary+" It is currently "+body.currently.temperature+" degree out.This high today is "+ body.daily.data[0].temperatureHigh+" with a low of "+body.daily.data[0].temperatureLow + ". There is a "+body.currently.precipIntensity+" % chance of rain")
         }
     })
 }
